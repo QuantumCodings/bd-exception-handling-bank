801097e603c6dde90f5b13c1537c25bb8b49a4ae
QuantumCodings <166426987+QuantumCodings@users.noreply.github.com>
2024-09-20 18:28:25 -0400

    Update CheckingAccount.java
    nice

--- src/com/amazon/ata/handlingexceptions/CheckingAccount.java
+++ src/com/amazon/ata/handlingexceptions/CheckingAccount.java
@@ -4,25 +4,10 @@ import java.math.BigDecimal;
 
 import com.amazon.ata.handlingexceptions.exceptions.InsufficientFundsException;
 
-/**
- * This class represents a checking bank account, which includes methods that 
- * allow money to be deposited or withdrawn from the account.
- */
 public class CheckingAccount implements BankAccount {
     
-    /**
-     * Identification number associated with the account
-     */
     private String accountId;
-    
-    /**
-     * Amount of money in the account.
-     */
     private BigDecimal balance;
-    
-    /*
-     * Validator helper class for validating input.
-     */
     private Validator validator = new Validator();
 
     public CheckingAccount(String accountId, BigDecimal balance) {
@@ -30,33 +15,28 @@ public class CheckingAccount implements BankAccount {
         this.balance = balance;
     }
 
-    /**
-     * Deposit amount to account.
-     * 
-     * @param amount of money to deposit
-     * @return value of account after the deposit
-     */
     @Override
     public BigDecimal deposit(BigDecimal amount) {
-        // TODO: Implement
-        return BigDecimal.ZERO;
+        if (validator.validate(amount)) {
+            balance = balance.add(amount);
+        }
+        return balance;
     }
 
-    /**
-     * Withdraw amount from account.
-     * 
-     * @param amount of money to withdraw
-     * @return value of account after the withdraw
-     * @throws InsufficientFundsException if account does not have enough funds to withdraw amount
-     */
     @Override
     public BigDecimal withdraw(BigDecimal amount) {
-        // TODO: implement
-        return BigDecimal.ZERO;
+        if (!validator.validate(amount)) {
+            throw new IllegalArgumentException("Invalid amount");
+        }
+        if (amount.compareTo(balance) > 0) {
+            throw new InsufficientFundsException("Insufficient funds for withdrawal");
+        }
+        balance = balance.subtract(amount);
+        return balance;
     }
 
     @Override
     public BigDecimal getBalance() {
-        // TODO: implement
-        return BigDecimal.ZERO;    }
+        return balance;
+    }
 }
