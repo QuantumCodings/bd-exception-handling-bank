4a66b231b5fc615d37d0ec8bd0d15bf677d64010
QuantumCodings <166426987+QuantumCodings@users.noreply.github.com>
2024-09-20 18:34:46 -0400

    Update Bank.java
    nice

--- src/com/amazon/ata/handlingexceptions/Bank.java
+++ src/com/amazon/ata/handlingexceptions/Bank.java
@@ -5,23 +5,22 @@ import java.math.BigDecimal;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-/**
- * This class represents a bank, which includes the functionality to transfer from
- * one BankAccount to another.
- */
 public class Bank {
     private Logger log = LogManager.getLogger(Bank.class);
 
-    /**
-     * Transfer money from one account to another. 
-     * 
-     * @param fromAccount BankAccount to withdraw amount from
-     * @param toAccount BankAccount to deposit amount into
-     * @param amount of money to transfer.
-     * @return true if transfer was successful, false if transfer fails due to insufficient funds
-     */
     public boolean transfer(BankAccount fromAccount, BankAccount toAccount, BigDecimal amount) {
-        // TODO: implement
-        return false;
+        try {
+            fromAccount.withdraw(amount);
+            toAccount.deposit(amount);
+            return true;
+        } catch (InsufficientFundsException e) {
+            log.error("Transfer failed due to insufficient funds: " + e.getMessage());
+            return false;
+        } catch (InvalidInputException e) {
+            throw e; // propagate invalid input exceptions
+        } catch (Exception e) {
+            log.error("An unexpected error occurred during transfer: " + e.getMessage());
+            return false;
+        }
     }
 }
